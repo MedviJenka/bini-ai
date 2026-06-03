d5e04ff11535b600ddd9418907a53dac07ab265a
Jenia Petrusenko <medvijenia@Jenias-MacBook-Pro.local>
2026-06-03 17:47:48 +0300

    decision points

--- backend/ai/agents/vision_agent/flow.py
+++ backend/ai/agents/vision_agent/flow.py
@@ -11,7 +11,7 @@ class ContentState(BaseModel):
     sample_image: Optional[Union[list, str]] = Field(default_factory=list)
     cache:        dict                       = {}
 
-class ContentFlow(Flow[ContentState]):
+class VisionFlow(Flow[ContentState]):
 
     @start()
     def run_vision_agent(self) -> dict:
@@ -19,19 +19,34 @@ class ContentFlow(Flow[ContentState]):
         return self.state.cache
 
     @router(run_vision_agent)
-    def generate_content(self):
-        return 'Proceed' if self.state.cache['final_decision']['confidence_level'] > 90 else 'Revaluate'
+    def decision_point_1(self) -> str:
+        return 'Success' if self.state.cache['final_decision']['confidence_level'] > 100 else 'Fail'
 
     @listen('Proceed')
-    def proceed_vision_agent(self): ...
-    @listen('Revaluate')
-    def revaluate_vision_agent(self): ...
+    def on_success(self) -> None:
+        import json
+        print(json.dumps(self.state.cache, indent=4))
 
-    @listen(generate_content)
-    def save_content(self):
-        print("Saving content")
-        output_dir = Path("output")
-        output_dir.mkdir(exist_ok=True)
-        with open(output_dir / "post.md", "w") as f:
-            f.write(self.state.final_post)
-        print("Post saved to output/post.md")
+    @listen('Fail')
+    def on_fail(self):
+        print(f'agent confidence level is {self.state.cache['final_decision']['confidence_level']}, re running')
+        return 'Fail'
+
+    @router(on_fail)
+    def decision_point_2(self) -> None:
+        self.run_vision_agent()
+
+    # @listeen('Proceed')
+    # def save_content(self):
+    #     print("Saving content")
+    #     output_dir = Path("output")
+    #     output_dir.mkdir(exist_ok=True)
+    #     with open(output_dir / "post.md", "w") as f:
+    #         f.write(f'{self.state.cache}')
+    #     print("Post saved to output/post.md")
+
+
+if __name__ == '__main__':
+    a = VisionFlow().kickoff(inputs={'prompt': 'is playwright displayed', 'image_path': r'/Users/medvijenia/dev/bini-ai/data/images/main.png'})
+    import json
+    print(json.dumps(a, indent=4))
